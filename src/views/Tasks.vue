a77d2feb8f1e6db1d58681891dfbd2341d585a83
chilot <https://github.com/Raigyo>
2021-09-15 14:11:18 +0200

    filter tasks display

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,7 +1,11 @@
 <template>
+  <!-- v-model = 2 ways binding -->
+  <input type="text" placeholder="Filtrer" v-model="letters" @keyup="filter" />
   <h3>Toutes les tâches</h3>
-  <div v-if="tasks.length > 0">
-    <div class="task" v-for="task in tasks" :key="task.id">
+  <!-- <div v-if="taskFiltered.length > 0"> -->
+  <!-- v-show uses css / v-if maniupulate DOM (ressources consuming) -->
+  <div v-show="taskFiltered.length > 0">
+    <div class="task" v-for="task in taskFiltered" :key="task.id">
       <h4>{{ task.name }}</h4>
       <p>{{ task.description }}</p>
       <p>Échéance: {{ convertTemporalityCase(task.temporality) }}</p>
@@ -16,13 +20,25 @@ export default {
   name: "Tasks",
   setup() {
     const tasks = ref([]);
+    const letters = ref("");
     tasks.value = tasksService.read();
+    let taskFiltered = ref([]);
 
     const convertTemporalityCase = (temporality) => {
       return tasksService.convertCase(temporality);
     };
-
-    return { tasks, convertTemporalityCase }; // to use fct in templates
+    const filter = () => {
+      // console.log(letters.value);
+      if (letters.value.length === 0) {
+        taskFiltered.value = tasks.value;
+      } else {
+        taskFiltered.value = tasks.value.filter((t) =>
+          t.name.toLocaleLowerCase().includes(letters.value.toLocaleLowerCase())
+        );
+      }
+    };
+    filter();
+    return { tasks, taskFiltered, convertTemporalityCase, letters, filter }; // to use fct in templates
   },
 };
 </script>
