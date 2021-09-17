edbf4b4f3ede10ec58ae3e8371b929caf3829f68
chilot <https://github.com/Raigyo>
2021-09-17 16:04:45 +0200

    adding dist subtree

---
+++ precache-manifest.d37c5989c70cfcdaedb032250037efc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfa30159c24381a2fb5",
+    "url": "/progressor-compo-api/css/about.97e7c7ab.css"
+  },
+  {
+    "revision": "6153ef3f6f7c42f7b91e",
+    "url": "/progressor-compo-api/css/app.3b13ea81.css"
+  },
+  {
+    "revision": "038825f99537357a143a59aa5e4df1ec",
+    "url": "/progressor-compo-api/index.html"
+  },
+  {
+    "revision": "edfa30159c24381a2fb5",
+    "url": "/progressor-compo-api/js/about.79108ffb.js"
+  },
+  {
+    "revision": "6153ef3f6f7c42f7b91e",
+    "url": "/progressor-compo-api/js/app.0320e636.js"
+  },
+  {
+    "revision": "a7b28d4ea76f0604a699",
+    "url": "/progressor-compo-api/js/chunk-vendors.782d657a.js"
+  },
+  {
+    "revision": "cf76d55569c76114bd57f28d884ae412",
+    "url": "/progressor-compo-api/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/progressor-compo-api/robots.txt"
+  }
+]);
